9d980a0c6b4500427200dea0c58f2ba678a80a68
Amine Ait Benmessaoud <80128117+AmineAitBenmessaoud@users.noreply.github.com>
2025-02-25 17:51:29 +0100

    adding components, docker still work to do

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, HomeComponent, HomeComponent, HeaderComponent],
+  template: `
+    <app-header></app-header>
+    <router-outlet></router-outlet> 
+    <app-home></app-home>
+  `
+  ,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
---
+++ src/app/components/greeting/greeting.component.ts
@@ -0,0 +1,11 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-greeting',
+  imports: [],
+  templateUrl: './greeting.component.html',
+  styleUrl: './greeting.component.scss'
+})
+export class GreetingComponent {
+    message = input("default greeting message"); // to receive input from another component or parent component
+}
